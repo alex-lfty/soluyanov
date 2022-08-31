1805650aea2874d5ca7d7c7df254d31f6503f811
Alex <alex@lfty.team>
2022-08-31 15:30:26 +0300

    Updated fundrise block

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,28 @@
 module.exports = {
   baseUrl: "https://soluyanov2022.ru",
   title: "Глеб Солуянов",
-  description: "Кандидат в муниципальные депутаты Москвы по району Измайлово Глеб Солуянов",
+  description:
+    "Кандидат в муниципальные депутаты Москвы по району Измайлово Глеб Солуянов",
   styles: ["fonts", "mvp", "main"],
-  programSlides: [1,2,3,4,5,6]
-}
+  programSlides: [1, 2, 3, 4, 5, 6],
+  fundrise: {
+    target: {
+      value: 70000,
+      toString() {
+        return this.value;
+      },
+      valueOf() {
+        return this.value;
+      },
+    },
+    current: {
+      value: 25549,
+      toString() {
+        return this.value;
+      },
+      valueOf() {
+        return this.value;
+      },
+    },
+  },
+};
